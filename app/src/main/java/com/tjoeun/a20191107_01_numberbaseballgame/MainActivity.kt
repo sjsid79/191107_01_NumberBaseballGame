b574be44869cafafae4e166534f81a8d31aacff8
sjsid79 <wjdtlr0723>
2019-11-07 20:57:08 +0900

    사용자 입력값을 자릿수별로 ArrayList에 저장

--- app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -14,8 +14,9 @@ class MainActivity : BaseActivity() {
     var chatList = ArrayList<ChatData>()
     var chatAdapter:ChatAdapter? = null
 
-    var questNumArray = ArrayList<Int>()
-
+    var questNumArray = ArrayList<Int>()        //컴퓨터가 낸 문제
+    var userInputNumArray = ArrayList<Int>()    //유저가 입력한 숫자의 배열
+            
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,13 +46,31 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    fun checkAnswer(){
+        
+        for(i in 0..2){
+            for (j in 0..2){
+                
+            }
+        }
+        
+    }
+    
     override fun setupEvents() {
         inputBtn.setOnClickListener {
             var inputNum = inputEdt.text.toString()
             chatList.add(ChatData(inputNum, "ME"))
 
             chatAdapter?.notifyDataSetChanged()
+
+            userInputNumArray.clear()                                   //들어있는 배열의값을 지운다.
+            userInputNumArray.add(inputNum.toInt() / 100)               //앞 숫자 추출
+            userInputNumArray.add((inputNum.toInt() / 10) % 10)         //가운데 숫자 추출
+            userInputNumArray.add(inputNum.toInt() % 10)                //뒤 숫자 추출 
+            
             chatListView.smoothScrollToPosition(chatList.size-1)
+            
+            checkAnswer()
         }
 
     }
