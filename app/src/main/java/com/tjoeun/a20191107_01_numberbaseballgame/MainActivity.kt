509d2c9b59d3037ee523e34bb0e79554e462fe84
sjsid79 <wjdtlr0723>
2019-11-07 20:34:42 +0900

    어댑터 / 리스트뷰와 연결

--- app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -3,10 +3,16 @@ package com.tjoeun.a20191107_01_numberbaseballgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeun.a20191107_01_numberbaseballgame.adapters.ChatAdapter
+import com.tjoeun.a20191107_01_numberbaseballgame.datas.ChatData
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
     // 471 -> 4,7,1
+
+    var chatList = ArrayList<ChatData>()
+    var chatAdapter:ChatAdapter? = null
 
     var questNumArray = ArrayList<Int>()
 
@@ -44,7 +50,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        chatAdapter = ChatAdapter(this, chatList)
+        chatListView.adapter = chatAdapter
     }
 
 }
---
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/adapters/ChatAdapter.kt
@@ -0,0 +1,33 @@
+package com.tjoeun.a20191107_01_numberbaseballgame.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tjoeun.a20191107_01_numberbaseballgame.R
+import com.tjoeun.a20191107_01_numberbaseballgame.datas.ChatData
+
+class ChatAdapter(context: Context, res:Int, list:ArrayList<ChatData>)
+    : ArrayAdapter<ChatData>(context, res, list){
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    constructor(context: Context, list: ArrayList<ChatData>) : this(context, R.layout.chat_list_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.chat_list_item, null)
+        }
+
+        // tempRow가 null인 경우를 위에서 대비했으니,
+        // row에서 tempRow는 절대 null 아니라고 하면서 대입
+        var row = tempRow!!     // !! -> tempRow는 절대 null이 아니라고 우기는거야.
+
+        return  row
+    }
+}
