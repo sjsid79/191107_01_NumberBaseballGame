a6e41140b11148b14e5f75f2db7c61e7907b4e2a
sjsid79 <wjdtlr0723>
2019-11-07 20:07:33 +0900

    컴퓨터 문제 출제

--- app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -2,22 +2,49 @@ package com.tjoeun.a20191107_01_numberbaseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
+    // 471 -> 4,7,1
+
+    var questNumArray = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupEvents()
         setValues()
+        createQuestion()
+    }
+
+    fun createQuestion(){
+        while (true){
+            val number = Random.nextInt(1,9)
+
+            //지금 들고 있는 숫자가 방금 나온 숫자가 아니면...
+            if (!questNumArray.contains((number))){
+                questNumArray.add(number)
+            }
+
+            //3개의 값을 다 받았으면 while를 빠져나와라..
+            if (questNumArray.size == 3) {
+                break
+            }
+        }
+
+        for(num in questNumArray) {
+            Log.d("출제 숫자", num.toString())
+        }
     }
 
     override fun setupEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun setValues() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
